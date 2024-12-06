a594db7e50a8515bfc71352b534479b3bcf0309f
Baer3 <b.epsteinreyes@tufts.edu>
2024-12-06 01:37:15 -0500

    Add files via upload

--- Final Project.py
+++ Final Project.py
@@ -1,9 +1,18 @@
-from machine import Pin
 from time import sleep
+import onewire
+from ds18x20 import DS18X20
 
-# You'll need to make sure the thermocouple.py library is copied to your ESP32
-from thermocouple import MAX31855 
+pin = Pin(1)
 
-# Now initialize the thermocouple object with the correct pins
-# Change the pin numbers to match your setup
-therm = MAX31855(cs=Pin(37), sck=Pin(38), so=Pin(36))
+#create the onewire object
+tempSensor = DS18X20(onewire.OneWire(pin))
+
+#scan for device addresses
+roms = tempSensor.scan()
+
+#print temperatures
+for i in range(100):
+    tempSensor.convert_temp()
+    sleep(.5)
+    for rom in roms:
+        print("Temperature: " + str(tempSensor.read_temp(rom)))
