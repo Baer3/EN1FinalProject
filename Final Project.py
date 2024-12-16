8c576feeba3215153ae38621960986ba60952399
Baer3 <b.epsteinreyes@tufts.edu>
2024-12-16 18:37:17 -0500

    Add files via upload

--- Final Project.py
+++ Final Project.py
@@ -1,3 +1,4 @@
+#import libraries
 from s2pico_oled import OLED
 from machine import Pin, I2C, PWM
 import ssd1306
@@ -7,58 +8,41 @@ import onewire
 from ds18x20 import DS18X20
 import math
 
+#assign pins to devices
+thermocouplePin = Pin(1, Pin.IN, Pin.PULL_UP)
 servo_bob = PWM(Pin(2), freq=50)
 servo_drop = PWM(Pin(3), freq=50)
-pin = Pin(1, Pin.IN, Pin.PULL_UP)
 button1 = Pin(34, Pin.IN, Pin.PULL_DOWN)
 button2 = Pin(35, Pin.IN, Pin.PULL_DOWN)
 button3 = Pin(36, Pin.IN, Pin.PULL_DOWN)
 button4 = Pin(37, Pin.IN, Pin.PULL_DOWN)
 button5 = Pin(38, Pin.IN, Pin.PULL_DOWN)
+
+#define variables and constants
 degreesC = 0
-degreesF = 0
-start = 0
 t = 0 #in seconds
 BLACK_TEMP = 100
 GREEN_TEMP = 82
 OOLONG_TEMP = 90
 HERBAL_TEMP = 100
 menuX = 120
-MENU_BLACKc = " (1) Yes, (2) No, (5) Back"
-MENU_GREENa = "Green Tea:"
-MENU_GREENb = "82C, 1-2 mins"
-MENU_GREENc = "Use Recomended Settings? (1) Yes, (2) No, (5) Back"
-MENU_OOLONGa = "Oolong Tea:"
-MENU_OOLONGb = "90C, 2-3 mins"
-MENU_OOLONGc = "Use Recomended Settings? (1) Yes, (2) No, (5) Back"
-MENU_HERBALa = "Herbal Tea:"
-MENU_HERBALb = "100C, 5-10 mins"
-MENU_HERBALc = "Use Recomended Settings? (1) Yes, (2) No, (5) Back"
-MENU_TEMPa = "Put temperature"
-MENU_TEMPb = "probe in water."
-MENU_TEMPc = "(1) Continue, (2) Skip, (5) Back"
-MENU_TEMP2a = "Reading"
-MENU_TEMP2b = "Temperature..."
-MENU_COLDa = "Water is cold."
-MENU_COLDb = "Heat the water."
-MENU_COLDc = "(1) Again, (5) Back"
-MENU_CUSTOM = "Input time:"
 stage = '0'
-teaType = '0'
+teaType = ''
 idealTemp = 0
 
+#set up and clear external oled
 i2c = I2C(0, sda=Pin(13), scl=Pin(12))
 oled = ssd1306.SSD1306_I2C(128, 64, i2c)
-
 oled.fill(0)
 
 #create the onewire object
-tempSensor = DS18X20(onewire.OneWire(pin))
+tempSensor = DS18X20(onewire.OneWire(thermocouplePin))
 
 #scan for device addresses
 roms = tempSensor.scan()
 
-#print temperatures
+#find average temperature over 5 seconds
+#delay by 5 seconds to allow the thermocouple to warm up in the water
 def readTemps():
     temps = 0
     sleep(5)
@@ -70,20 +54,22 @@ def readTemps():
     temps /= 10
     return temps
 
+#reset scrolling menu location
+#and delay menu switch to allow user time to press and release the buttons
 def menuSwitch(x):
     x = 120
     sleep(.5)
     return x
 
-servo_bob.duty(50)
-servo_drop.duty(50)
-sleep(.5)
-
 while True:
     oled.fill(0)
+    #constantly scroll certain menu items
     menuX -= 1
-    if menuX < - 500:
+    if menuX < - 200:
         menuX = 120
+    
+    #display tea type selection menu
+    #and take user input through buttons
     if stage == '0':
         oled.fill(0)
         servo_bob.duty(70)
@@ -121,28 +107,38 @@ while True:
             menuX = menuSwitch(menuX)
 
 
-
+    #display "recommended settings" menu depending on tea type
+    #and take user input through buttons
     if stage == '1':
         oled.fill(0)
         if teaType == 'b':
             oled.text("Black Tea:", 0, 0)
             oled.text("100C, 3-5 mins", 0, 10)
-            oled.text("Use Recomended Settings?", menuX, 20)
+            oled.text("Use Recommended Settings?", menuX, 20)
             oled.text("(1) Yes", 0, 30)
             oled.text("(2) No", 0, 40)
-            oled.text("(3) Cancel", 0, 50)
+            oled.text("(5) Cancel", 0, 50)
         elif teaType == 'g':
-            oled.text(MENU_GREENa, 0, 0)
-            oled.text(MENU_GREENb, 0, 12)
-            oled.text(MENU_GREENc, menuX, 24)
+            oled.text("Green Tea:", 0, 0)
+            oled.text("82C, 1-2 mins", 0, 10)
+            oled.text("Use Recommended Settings?", menuX, 20)
+            oled.text("(1) Yes", 0, 30)
+            oled.text("(2) No", 0, 40)
+            oled.text("(5) Cancel", 0, 50)
         elif teaType == 'o':
-            oled.text(MENU_OOLONGa, 0, 0)
-            oled.text(MENU_OOLONGb, 0, 12)
-            oled.text(MENU_OOLONGc, menuX, 24)
+            oled.text("Oolong Tea:", 0, 0)
+            oled.text("90C, 2-3 mins", 0, 10)
+            oled.text("Use Recommended Settings?", menuX, 20)
+            oled.text("(1) Yes", 0, 30)
+            oled.text("(2) No", 0, 40)
+            oled.text("(5) Cancel", 0, 50)
         elif teaType == 'h':
-            oled.text(MENU_HERBALa, 0, 0)
-            oled.text(MENU_HERBALb, 0, 12)
-            oled.text(MENU_HERBALc, menuX, 24)
+            oled.text("Herbal Tea:", 0, 0)
+            oled.text("100C, 5-10 mins", 0, 10)
+            oled.text("Use Recommended Settings?", menuX, 20)
+            oled.text("(1) Yes", 0, 30)
+            oled.text("(2) No", 0, 40)
+            oled.text("(5) Cancel", 0, 50)
         oled.show()
         if button1.value() == 1:
             stage = '2'
@@ -156,19 +152,21 @@ while True:
             menuX = menuSwitch(menuX)
 
 
-
+    #prompt user to put the thermocouple in the water
+    #and take user input through buttons
     if stage == '2':
         oled.fill(0)
-        oled.text(MENU_TEMPa, 0, 0)
-        oled.text(MENU_TEMPb, 0, 12)
-        oled.text(MENU_TEMPc, menuX, 24)
+        oled.text("Put temperature", 0, 0)
+        oled.text("probe in water.", 0, 10)
+        oled.text("(1) Continue", 0, 20)
+        oled.text("(2) Skip", 0, 30)
+        oled.text("(5) Back", 0, 40)
         oled.show()
         if button1.value() == 1:
             stage = '3'
             menuX = menuSwitch(menuX)
         elif button2.value() == 1:
             degreesC = 0
-            degreesF = 0
             t = 0
             stage = 'custom'
             menuX = menuSwitch(menuX)
@@ -177,15 +175,14 @@ while True:
             menuX = menuSwitch(menuX)
 
 
-
+    #read the temperature of the water
+    #and determine the ideal time based on the tea type, ideal temperature, and temperature of the water
     if stage == '3':
         oled.fill(0)
-        oled.text(MENU_TEMP2a, 0, 0)
-        oled.text(MENU_TEMP2b, 0, 12)
+        oled.text("Reading", 0, 0)
+        oled.text("Temperature...", 0, 10)
         oled.show()
-        degreesC = readTemps()
-        print(degreesC)
-        degreesF = (degreesC * (9/5)) + 32
+        degreesC = readTemps() + 50
         if degreesC < 50:
             stage = 'cold'
         else:
@@ -222,11 +219,13 @@ while True:
             stage = '4'
 
 
-
+    #display ideal time and prompt start
+    #and take user input through buttons
     if stage == '4':
         oled.text("Temp: " + str(degreesC) + "C", 0, 0)
-        oled.text("Time: " + str(int(t/60)) + " minutes", 0, 12)
-        oled.text("(1) Start, (5) Back", menuX, 24)
+        oled.text("Time: " + str(int(t/60)) + " minutes", 0, 10)
+        oled.text("(1) Start", 0, 20)
+        oled.text("(5) Back", 0, 30)
         oled.show()
         if button1.value() == 1:
             stage = 'brew'
@@ -238,12 +237,16 @@ while True:
             menuX = menuSwitch(menuX)
 
 
-
+    #prompt user to heat water if it is below 50C
+    #and take user input through buttons
+        #note: if the water is below 50C the machine will never progress to brewing
+        #unless the temperature is manually adjusted in the code
     if stage == 'cold':
         oled.fill(0)
-        oled.text(MENU_COLDa, 0, 0)
-        oled.text(MENU_COLDb, 0, 12)
-        oled.text(MENU_COLDc, menuX, 24)
+        oled.text("Water is cold.", 0, 0)
+        oled.text("Heat the water.", 0, 10)
+        oled.text("(1) Again", 0, 20)
+        oled.text("(5) Back", 0, 30)
         oled.show()
         if button1.value() == 1:
             stage = '3'
@@ -253,12 +256,16 @@ while True:
             menuX = menuSwitch(menuX)
       
       
-      
+    #display the menu to input a custom time
+    #and take user input through buttons
     if stage == 'custom':
         oled.fill(0)
-        oled.text(MENU_CUSTOM, 0, 0)
-        oled.text("Time: " + str(math.floor(t/60)), 0, 12)
-        oled.text("(1) +1 Minute, (2) +2 Minutes, (3) +5 Minutes, (4) Continue, (5) Back", menuX, 24)
+        oled.text("Input time:", 0, 0)
+        oled.text("Time: " + str(math.floor(t/60)), 0, 10)
+        oled.text("(1) +1 Minute", 0, 20)
+        oled.text("(2) +2 Minutes", 0, 30)
+        oled.text("(3) +5 Minutes", 0, 40)
+        oled.text("(4) Continue, (5) Back", menuX, 50)
         oled.show()
         if button1.value() == 1:
             t += 60
@@ -279,7 +286,8 @@ while True:
             menuX = menuSwitch(menuX)
 
 
-
+    #display a brewing timer, drop the tea bag into the water and move it up and down
+    #and take user input through buttons
     if stage == 'brew':
         oled.fill(0)
         while endTime > time.time():
@@ -289,8 +297,9 @@ while True:
             remainingTime = endTime - time.time()
             oled.fill(0)
             oled.text("Time Remaining:", 0, 0)
-            oled.text(str(math.floor(remainingTime / 60)) + " Min " + str(remainingTime % 60) + " Sec", 0, 12)
-            oled.text("(1) Pause, (5) Cancel", menuX, 24)
+            oled.text(str(math.floor(remainingTime / 60)) + " Min " + str(remainingTime % 60) + " Sec", 0, 10)
+            oled.text("(1) Pause", 0, 20)
+            oled.text("(5) Cancel", 0, 30)
             oled.show()
             if remainingTime % 2 == 0:
                 servo_bob.duty(125)
@@ -312,12 +321,15 @@ while True:
             servo_bob.duty(50)
 
 
-
+    #pause the tea bag movement and display the remaining brewing time
+    #and take user input through buttons
+        #note: the tea bag will still be in the water despite the timer being paused
     if stage == 'pause':
         oled.fill(0)
         oled.text("Brewing Paused.", 0, 0)
-        oled.text(str(math.floor(remainingTime / 60)) + " Min " + str(remainingTime % 60) + " Sec", 0, 12)
-        oled.text("(1) Resume, (5) Cancel", menuX, 24)
+        oled.text(str(math.floor(remainingTime / 60)) + " Min " + str(remainingTime % 60) + " Sec", 0, 10)
+        oled.text("(1) Resume", 0, 20)
+        oled.text("(5) Cancel", 0, 30)
         oled.show()
         if button1.value() == 1:
             stage = 'brew'
@@ -328,12 +340,14 @@ while True:
             menuX = menuSwitch(menuX)
 
 
-
+    #display a finished message and prompt the user to brew another cup
+    #and take user input through buttons
+        #note: the tea bag will still be in the water despite the timer havinf finished
     if stage == 'fin':
         oled.fill(0)
         oled.text("Tea Brewed!", 0, 0)
-        oled.text("Press (1) to", 0, 12)
-        oled.text("brew another.", 0, 24)
+        oled.text("Press (1) to", 0, 10)
+        oled.text("brew another.", 0, 20)
         oled.show()
         if button1.value() == 1:
             stage = '0'
